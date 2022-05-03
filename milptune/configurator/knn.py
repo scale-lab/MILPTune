c881990535b37d4a75cc869d66268c10e57b8eca
abdelrahmanhosny <abdelrahman.hosny@hotmail.com>
2022-05-02 22:26:43 -0400

    update knn to work with new db schema

--- milptune/configurator/knn.py
+++ milptune/configurator/knn.py
@@ -1,6 +1,9 @@
+import os
+
 import metric_learn
 import numpy as np
 from joblib import load
+from sklearn.manifold import TSNE
 from sklearn.neighbors import KNeighborsTransformer
 
 from milptune.db.connections import get_client
@@ -18,16 +21,21 @@ def _check_(test, array):
 
 
 def get_configuration_parameters(instance_file: str, dataset_name: str, n_neighbors=5, n_configs=5):
+    np.random.seed(10)
     # 0. Connect to database
     client = get_client()
     db = client.milptunedb
     dataset = db["milptune_metadata"]
 
     # 1. Load metric learning model
-    r = dataset.find_one({f"{dataset_name}.mlkr.model": {"$exists": True}})
+    r = dataset.find_one({f"{dataset_name}.model": {"$exists": True}})
     if not r:
-        raise Exception("Cannot find saved model file")
-    mlkr: metric_learn.MLKR = load(r[dataset_name]["mlkr"]["model"])
+        raise Exception("Cannot find trained model")
+    mlkr: metric_learn.MLKR = load(os.path.expanduser(r[dataset_name]["model"]["mlkr"]))
+    mask = np.load(os.path.expanduser(r[dataset_name]["model"]["mask"]))
+    tsne = None
+    if r[dataset_name]["model"].get("tsne", None):
+        tsne: TSNE = load(os.path.expanduser(r[dataset_name]["model"]["tsne"]))
 
     # 2. Transform instance to new metric space
     r = dataset.find_one(
@@ -39,11 +47,14 @@ def get_configuration_parameters(instance_file: str, dataset_name: str, n_neighb
     if not r:
         raise Exception(f"Cannot load metadata for {dataset_name}")
 
-    mask = from_mongo_binary(r[dataset_name]["mask"])
     A = get_A(instance_file, r[dataset_name]["vars_index"], r[dataset_name]["conss_index"])
     X = np.asarray(A.todense().flatten())
     X = X.reshape(1, -1)
     X = np.delete(X, mask, axis=1)
+
+    if tsne:
+        X = tsne.fit_transform(X)
+
     X_mlkr = mlkr.transform(X)
 
     # 3. Load full data to get knn
@@ -77,7 +88,7 @@ def get_configuration_parameters(instance_file: str, dataset_name: str, n_neighb
 
     # 6. Suggest `n_configs` configurations with lowest cost from all neighbors
     suggested = list(zip(configs, config_distances))
-    suggested = sorted(suggested, key=lambda c: c[0]["cost"])
+    suggested = sorted(suggested, key=lambda c: (c[0]["cost"], c[1]))
     suggested_configs, distances = zip(*suggested)
 
     return suggested_configs, distances
